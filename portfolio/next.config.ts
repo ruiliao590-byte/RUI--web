4db8c2f803463d61b9deddd8ed8d00f44c7621ce
RUI <ruiliao590@gmail.com>
2026-03-25 21:44:54 +0800

    web

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repo = 'RUI--web';
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
   reactCompiler: true,
 };
 
